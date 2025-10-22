1fba4dafba6e0e559cab262793defa398bfddfd2
maria0fernanda0 <mafe.dias06@gmail.com>
2025-10-22 18:02:03 -0300

    primeira aula de java

---
+++ src/main/java/PrimeiroExercicio.java
@@ -0,0 +1,9 @@
+public class PrimeiroExercicio {
+
+    public static void main (String args[]){
+
+        System.out.println("exibindo um texto qualquer.");
+        System.out.println("Hello world!");
+
+    }
+}
